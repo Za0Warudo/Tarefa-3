6e35a138e576d84efb048b1fc5664e62d2c7d428
Za0Warudo <139021935+Za0Warudo@users.noreply.github.com>
2023-07-08 21:03:46 -0300

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,20 @@
+function add(first_number, second_number){
+    return first_number + second_number
+}
+function sub(first_number, second_number){
+    return first_number - second_number 
+}
+function mul(first_number, second_number){
+    return first_number * second_number
+}
+function div(first_number, second_number){
+    return first_number / second_number
+}
+function is_even(number){
+    if (number % 2 == 0 ){
+        return true 
+    }
+    else{
+        return false
+    }
+}
